cdc0e25d1b52f36be26d9bd47ada15f0fb658aa8
Kevin Crawford <crawfordkevin101@gmail.com>
2021-07-10 19:57:16 -0400

    added jquery and moment

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 
+// function headerDate() {
+//     var currentHeaderDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+//     $(".date-container").text(currentHeaderDate);
+//     console.log(headerDate)
+// }
+
+// headerDate();
+
+// setInterval(() => {
+//     const time = new Date();
+//     const month = time.getMonth();
+//     const date = time.getDate();
+//     const day = time.getDay();
+
+// }, 1000)
