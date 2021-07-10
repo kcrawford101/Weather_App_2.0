c91168bcb2aafca1de8f37b97b224c62f4a7b0fc
Kevin Crawford <crawfordkevin101@gmail.com>
2021-07-10 19:22:27 -0400

    added item elements into js file

---
+++ script.js
@@ -0,0 +1,8 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherEl = document.getElementById('currentWeather');
+const timeZone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
