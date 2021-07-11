bd4265f66c25822c04e3133ff5828e7536327aef
Kevin Crawford <crawfordkevin101@gmail.com>
2021-07-10 21:01:27 -0400

    added fetch API for weather and current time and date moment function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// API Key
+const apiKey= 'b97e0971f053e47fa5a7a92a78b0c889';
+
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
 const currentWeatherEl = document.getElementById('currentWeather');
@@ -6,18 +9,31 @@ const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 
-// function headerDate() {
-//     var currentHeaderDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-//     $(".date-container").text(currentHeaderDate);
-//     console.log(headerDate)
-// }
+// Current Date & Time
+headerDate();
+function headerDate() {
+    var currentTime= moment().format('LT');
+    $(".time").text(currentTime);
 
-// headerDate();
+    var currentDate= moment().format('dddd MMM Do');
+    $(".date").text(currentDate);
 
-// setInterval(() => {
-//     const time = new Date();
-//     const month = time.getMonth();
-//     const date = time.getDate();
-//     const day = time.getDay();
+    console.log(headerDate);
+};
 
-// }, 1000)
+// API Fetch Request
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        
+        let {latitude, longitude } = success.coords;
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${apiKey}`)
+        .then(res => res.json()).then(data => {
+        console.log(data)
+        // showWeatherData(data);
+        })
+
+    })
+}
+
