9e0b8f8f9f1ac8d6d8b1f8fa3fc192b67b3d7cfb
Kevin Crawford <crawfordkevin101@gmail.com>
2021-07-12 21:55:41 -0400

    added search weather function

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const timeZone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
+const search = document.getElementById('search')
+// const weatherList = document.getElementById('weather-list')
 
 // Current Date & Time
 headerDate();
@@ -22,71 +24,98 @@ function headerDate() {
 };
 
 // API Fetch Request
-getWeatherData();
-function getWeatherData () {
-    navigator.geolocation.getCurrentPosition((success) => {
-        
-        let {latitude, longitude } = success.coords;
+// getWeatherData();
 
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${apiKey}`)
-        .then(res => res.json()).then(data => {
-        console.log(data)
-        displayWeather(data);
-        });
-        // Display Function
-        function displayWeather (data) {
-
-            timeZone.innerHTML = data.timezone;
-            countryEl.innerHTML =data.lat + 'N ' + data.lon + 'E ';
-                       
-            // const { name } = data;
-            const  weather  = data.current.weather[0].main;
-            const { temp, humidity } = data.current;
-            const { wind_speed } = data.current;
-            const { uvi } = data.current;                    
+// Weather Variable
+let weather = {
+    // Fetch Current Weather Function
+     getWeatherData: function() {
+        navigator.geolocation.getCurrentPosition((success) => {
             
-            // document.querySelector(".city").innerText = "Weather in " + name;
-            // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-            document.querySelector(".condition").innerText = weather;
-            document.querySelector(".temp").innerText = temp + "°C";
-            document.querySelector(".humid").innerText = humidity + "%";
-            document.querySelector(".wind").innerText = wind_speed + "km/h";
-            document.querySelector(".uvi").innerText = uvi + '%';
-
-            // 5 Day Weather Forecast Display Loop
-            let otherDayForecast = ''
-            data.daily.forEach((day, idx) => {
-                if(idx == 0){
-                    currentTempEl.innerHTML = `
-                    <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="other">
-                    <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-                    <div class="temp">Night - ${day.temp.night}°C</div>
-                    <div class="temp">Day - ${day.temp.day}C</div>
-                </div> `
-
-                }else{
-                    otherDayForecast += `
-                    <div class="weather-forecast-item">
-                    <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-                    <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">                    
-                    <div class="temp">Night: ${day.temp.night}°C</div>
-                    <div class="temp">Day: ${day.temp.day}°C</div>                    
-                    </div>`
-                }
-            })
-            weatherForecastEl.innerHTML = otherDayForecast
-
-
-        
-            
-        }
-        
-
-    });
+            let {latitude, longitude } = success.coords;
     
-       
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${apiKey}`)
+            .then(res => res.json()).then(data => {
+            console.log(data)
+            displayWeather(data);
+            });
+            // Display Current Weather Function
+            function displayWeather (data) {
+    
+                timeZone.innerHTML = data.timezone;
+                countryEl.innerHTML =data.lat + 'N ' + data.lon + 'E ';
+                           
+                // const { name } = data;
+                const  weather  = data.current.weather[0].main;
+                // Get Icon
+                const { temp, humidity } = data.current;
+                const { wind_speed } = data.current;
+                const { uvi } = data.current;                    
+                
+                // document.querySelector(".city").innerText = "Weather in " + name;
+                // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+                document.querySelector(".condition").innerText = weather;
+                document.querySelector(".temp").innerText = temp + "°C";
+                document.querySelector(".humid").innerText = humidity + "%";
+                document.querySelector(".wind").innerText = wind_speed + "km/h";
+                document.querySelector(".uvi").innerText = uvi + '%';
+    
+                // 5 Day Weather Forecast Display Loop
+                let otherDayForecast = ''
+                data.daily.forEach((day, idx) => {
+                    if(idx == 0){
+                        currentTempEl.innerHTML = `
+                        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                    <div class="other">
+                        <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                        <div class="temp">Night - ${day.temp.night}°C</div>
+                        <div class="temp">Day - ${day.temp.day}C</div>
+                    </div> `
+    
+                    }else{
+                        otherDayForecast += `
+                        <div class="weather-forecast-item">
+                        <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">                    
+                        <div class="temp">Night: ${day.temp.night}°C</div>
+                        <div class="temp">Day: ${day.temp.day}°C</div>                    
+                        </div>`
+                    }
+                })
+                weatherForecastEl.innerHTML = otherDayForecast                 
+                
+            };       
+    
+        });
+        
+           
+    }
 };
+
+let queryURL= `api.openweathermap.org/data/2.5/weather?q={city name}&appid=${apiKey}`;
+
+let weatherSearch = {
+    getWeatherData: function (city) {
+        fetch(queryURL) 
+            .then ((response) => response.json())
+            .then ((data) => this.displayWeather(data))
+    },
+    
+
+    
+}
+document.querySelector(".search-bar button").addEventListener("click", function() {
+    weatherSearch.search();
+});
+document.querySelector(".search").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weatherSearch.search();
+    }
+});
+weather.getWeatherData("")
+
+
+
 
 
 
